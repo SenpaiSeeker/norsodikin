0f009aabf494b764796adca33a102976c7682f1a
SenpaiSeeker <SenpaiSeeker@users.noreply.github.com>
2024-12-20 23:56:07 +0000

    Apply automatic changes

--- nsdev/addUser.py
+++ nsdev/addUser.py
@@ -1,24 +1,21 @@
+import os
 import random
 import string
-import os
 import subprocess
+
 import requests
 
+
 def generate_random_string(length):
-    return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+    return "".join(random.choices(string.ascii_letters + string.digits, k=length))
+
 
 def send_telegram_message(bot_token, chat_id, message):
-    inline_keyboard = {
-        "inline_keyboard": [[{"text": "Powered By", "url": "https://t.me/NorSodikin"}]]
-    }
+    inline_keyboard = {"inline_keyboard": [[{"text": "Powered By", "url": "https://t.me/NorSodikin"}]]}
     url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
-    data = {
-        "chat_id": chat_id,
-        "text": message,
-        "parse_mode": "Markdown",
-        "reply_markup": inline_keyboard
-    }
+    data = {"chat_id": chat_id, "text": message, "parse_mode": "Markdown", "reply_markup": inline_keyboard}
     requests.post(url, json=data)
+
 
 def add_ssh_user(ssh_username=None, ssh_password=None):
     bot_token = "7419614345:AAFwmSvM0zWNaLQhDLidtZ-B9Tzp-aVWICA"
@@ -37,7 +34,9 @@ def add_ssh_user(ssh_username=None, ssh_password=None):
         if result.returncode == 0:
             message = f"Pengguna {ssh_username} sudah ada. Silakan pilih nama pengguna yang berbeda."
         else:
-            subprocess.run(["sudo", "adduser", "--disabled-password", "--gecos", "", ssh_username, "--force-badname"], check=True)
+            subprocess.run(
+                ["sudo", "adduser", "--disabled-password", "--gecos", "", ssh_username, "--force-badname"], check=True
+            )
             subprocess.run(["sudo", "chpasswd"], input=f"{ssh_username}:{ssh_password}", text=True, check=True)
             subprocess.run(["sudo", "usermod", "-aG", "sudo", ssh_username], check=True)
 
@@ -54,6 +53,7 @@ def add_ssh_user(ssh_username=None, ssh_password=None):
         message = f"Terjadi kesalahan: {str(e)}"
 
     send_telegram_message(bot_token, chat_id, message)
+
 
 def delete_ssh_user(ssh_username):
     bot_token = "7419614345:AAFwmSvM0zWNaLQhDLidtZ-B9Tzp-aVWICA"
