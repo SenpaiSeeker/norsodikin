6dfe961a83e510924ab10a5f0e100aaa6770813f
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2025-01-02 16:22:23 +0700

    Update storekey.py

--- nsdev/storekey.py
+++ nsdev/storekey.py
@@ -3,42 +3,34 @@ class KeyManager:
         self.argparse = __import__("argparse")
         self.tempfile = __import__("tempfile")
         self.os = __import__("os")
-        self.log = __import__("nsdev").logger.LoggerHandler()
+        self.sys = __import__("sys")
+        self.logger = __import__("nsdev").logger.LoggerHandler()
         self.temp_file = self.os.path.join(self.tempfile.gettempdir(), "temp_key.txt")
 
     def save_key(self, key):
         try:
             with open(self.temp_file, "w") as f:
                 f.write(key)
-            self.log.info(f"Key '{key}' telah disimpan.")
-            return key
         except OSError as e:
-            self.log.error(f"Terjadi kesalahan saat menyimpan key: {e}")
-            return None
+            self.logger.error(f"Terjadi kesalahan saat menyimpan key: {e}")
+            self.sys.exit(1)
 
     def read_key(self):
         if self.os.path.exists(self.temp_file):
             try:
                 with open(self.temp_file, "r") as f:
                     saved_key = f.read().strip()
-                self.log.debug(f"Key terakhir yang disimpan: {saved_key}")
-                return saved_key
+                return saved_key 
             except OSError as e:
-                self.log.error(f"Terjadi kesalahan saat membaca key: {e}")
-                return None
+                self.logger.error(f"Terjadi kesalahan saat membaca key: {e}")
+                self.sys.exit(1)
         else:
-            self.log.warning("Tidak ada key yang disimpan. Jalankan ulang program dengan --key atau --new-key.")
-            return None
+            self.logger.warning("Tidak ada key yang disimpan. Jalankan ulang program dengan --key")
+            self.sys.exit(1)
 
     def handle_arguments(self):
         parser = self.argparse.ArgumentParser()
-        parser.add_argument("--key", type=str)
-        parser.add_argument("--new-key", type=str)
+        parser.add_argument("--key", type=str, help="Key yang ingin disimpan atau digunakan.")
         args = parser.parse_args()
 
-        if args.key:
-            return self.save_key(args.key)
-        elif args.new_key:
-            return self.save_key(args.new_key)
-        else:
-            return self.read_key()
+        return self.save_key(args.key) if args.key else self.read_key(
