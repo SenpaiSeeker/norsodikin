2e573632386c0f90bd89648cf89db919ee8666a5
SenpaiSeeker <SenpaiSeeker@users.noreply.github.com>
2025-01-13 17:18:27 +0000

    Apply automatic changes

--- nsdev/argument.py
+++ nsdev/argument.py
@@ -1,11 +1,15 @@
 class Argument:
     def __init__(self):
         self.pyrogram = __import__("pyrogram")
-        
+
     async def getUserId(self, message, username):
         if entities := message.entities:
             entity = entities[1 if message.text.startswith("/") else 0]
-            return (await message._client.get_chat(username)).id if entity.type == self.pyrogram.enums.MessageEntityType.MENTION else entity.user.id if entity.type == self.pyrogram.enums.MessageEntityType.TEXT_MENTION else username
+            return (
+                (await message._client.get_chat(username)).id
+                if entity.type == self.pyrogram.enums.MessageEntityType.MENTION
+                else entity.user.id if entity.type == self.pyrogram.enums.MessageEntityType.TEXT_MENTION else username
+            )
         return username
 
     async def userId(self, message, text):
@@ -13,7 +17,7 @@ class Argument:
 
     async def getReasonAndId(self, message, sender_chat=False):
         text, args, reply = message.text.strip(), message.text.split(), message.reply_to_message
-        
+
         if reply:
             user_id = (reply.from_user.id if reply.from_user else reply.sender_chat.id) if sender_chat and reply.sender_chat and reply.sender_chat.id != message.chat.id else None
             return (user_id, text.split(None, 1)[1] if len(args) > 1 else None) if user_id else (None, None)
@@ -21,7 +25,10 @@ class Argument:
         return (await self.userId(message, args[1]), None) if len(args) == 2 else (await self.userId(message, args[1]), " ".join(args[2:])) if len(args) > 2 else (None, None)
 
     async def getAdmin(self, message):
-        return (await message._client.get_chat_member(message.chat.id, message.from_user.id)).status in (self.pyrogram.enums.ChatMemberStatus.ADMINISTRATOR, self.pyrogram.enums.ChatMemberStatus.OWNER)
+        return (await message._client.get_chat_member(message.chat.id, message.from_user.id)).status in (
+            self.pyrogram.enums.ChatMemberStatus.ADMINISTRATOR,
+            self.pyrogram.enums.ChatMemberStatus.OWNER,
+        )
 
     async def getId(self, message):
         return (await self.getReasonAndId(message))[0]
