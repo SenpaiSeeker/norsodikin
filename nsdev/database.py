23b09784fd68fcef5927ed21abff387ac70bc01e
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2025-04-09 00:51:08 +0700

    Update database.py

--- nsdev/database.py
+++ nsdev/database.py
@@ -130,10 +130,17 @@ class DataBase:
             encrypted_data = result.get(var_key, {}) if result else {}
         else:
             encrypted_data = self._load_data().get("vars", {}).get(str(user_id), {}).get(var_key, {})
-        return {
-            key: ([self.cipher.decrypt(v) for v in value] if isinstance(value, list) else self.cipher.decrypt(value) if isinstance(value, str) else value)
+
+        decrypted = {
+            key: (
+                [self.cipher.decrypt(v) for v in value] if isinstance(value, list)
+                else self.cipher.decrypt(value) if isinstance(value, str)
+                else value
+            )
             for key, value in encrypted_data.items()
         }
+
+        return self.json.dumps(decrypted, indent=4)
 
     def setExp(self, user_id, exp=30):
         have_exp = self.getVars(user_id, "EXPIRED_DATE")
