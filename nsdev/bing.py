d696245031fef61a7013de8572662c3a3f851901
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2025-05-21 13:52:07 +0700

    Update bing.py

--- nsdev/bing.py
+++ nsdev/bing.py
@@ -1,4 +1,4 @@
-class AsyncImageGenerator(__import__("nsdev").LoggerHandler):
+class ImageGenerator(__import__("nsdev").LoggerHandler):
     def __init__(self, auth_cookie_u: str, auth_cookie_srchhpgusr: str, logging_enabled: bool = True):
         self.httpx = __import__("httpx")
         self.re = __import__("re")
