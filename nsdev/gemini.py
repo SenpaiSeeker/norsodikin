fadc8124af5321f0fdbb42996fe64d861e4952d7
SenpaiSeeker <SenpaiSeeker@users.noreply.github.com>
2025-05-26 16:08:44 +0000

    Apply automatic changes

--- nsdev/gemini.py
+++ nsdev/gemini.py
@@ -27,11 +27,7 @@ class ChatbotGemini:
                 "Mari kita ngobrol dengan suasana santai dan menyenangkan!"
             )
 
-        return self.genai.GenerativeModel(
-            model_name="gemini-2.0-flash-exp",
-            generation_config=self.generation_config,
-            system_instruction=instruction
-        )
+        return self.genai.GenerativeModel(model_name="gemini-2.0-flash-exp", generation_config=self.generation_config, system_instruction=instruction)
 
     def send_chat_message(self, message, user_id, bot_name):
         history = self.chat_history.setdefault(user_id, [])
