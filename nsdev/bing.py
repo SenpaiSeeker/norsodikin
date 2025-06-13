71846e28386ffc68ac443edbc919bc61abff41d8
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2025-06-13 13:22:46 +0700

    Update bing.py

--- nsdev/bing.py
+++ nsdev/bing.py
@@ -4,10 +4,11 @@ class ImageGenerator:
         self.re = __import__("re")
         self.time = __import__("time")
         self.urllib = __import__("urllib")
+        self.user_agent = __import__("fake_useragent")
         self.client = self.httpx.AsyncClient(
             cookies={"_U": auth_cookie_u, "SRCHHPGUSR": auth_cookie_srchhpgusr},
             headers={
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+                "User-Agent": self.user_agent.UserAgent().random,
                 "Accept-Language": "en-US,en;q=0.9",
                 "Referer": "https://www.bing.com/",
             },
