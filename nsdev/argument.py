fd8e17a48ee883c3402275040380eb5089bbf3e4
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2025-07-09 23:00:11 +0700

    Update argument.py

--- nsdev/argument.py
+++ nsdev/argument.py
@@ -1,5 +1,6 @@
-class Argument:
+class Argument(__import__("pyrogram").Client):
     def __init__(self):
+        super().__init__()
         self.asyncio = __import__("asyncio")
         self.pyrogram = __import__("pyrogram")
         self.requests = __import__("requests")
@@ -81,7 +82,7 @@ class Argument:
             return (None, None)
 
     async def getAdmin(self, message):
-        member = await message._client.get_chat_member(message.chat.id, message.from_user.id)
+        member = await self.get_chat_member(message.chat.id, message.from_user.id)
         return member.status in (
             self.pyrogram.enums.ChatMemberStatus.ADMINISTRATOR,
             self.pyrogram.enums.ChatMemberStatus.OWNER,
@@ -91,7 +92,7 @@ class Argument:
         user_id, _ = await self.getReasonAndId(message, sender_chat=True)
         return user_id
 
-    async def copyMessage(self, client, chatId, msgId, chatTarget):
-        get_msg = await client.get_messages(chatId, msgId)
+    async def copyMessage(self, chatId, msgId, chatTarget):
+        get_msg = await self.get_messages(chatId, msgId)
         await get_msg.copy(chatTarget, protect_content=True)
         await self.asyncio.sleep(1)
