09327d38e46b4d513f8d810c350554ed9dd177ab
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2025-07-13 13:06:23 +0700

    Update __init__.py

--- nsdev/__init__.py
+++ nsdev/__init__.py
@@ -14,7 +14,7 @@ from .payment import PaymentMidtrans, PaymentTripay, VioletMediaPayClient
 from .storekey import KeyManager
 from .ymlreder import YamlHandler
 
-__version__ = "0.8.6"
+__version__ = "0.8.7"
 __author__ = "@NorSodikin"
 
 
