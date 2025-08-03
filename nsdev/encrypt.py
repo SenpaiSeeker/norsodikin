6f9d6bac4503c60b8a6c19ce0b116c314d302eb0
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2025-08-03 21:18:07 +0700

    Update encrypt.py

--- nsdev/encrypt.py
+++ nsdev/encrypt.py
@@ -148,37 +148,34 @@ class CipherHandler:
 
         to_hex = lambda s: s.encode("utf-8").hex()
         hex_map = {
-            "n": to_hex("nsdev"),
-            "C": to_hex("CipherHandler"),
-            "b": to_hex("builtins"),
-            "t": to_hex("types"),
-            "g": to_hex("globals"),
-            "i": to_hex("__import__"),
-            "a": to_hex("getattr"),
-            "c": to_hex("compile"),
-            "f": to_hex("FunctionType"),
+            "n": to_hex("nsdev"), "C": to_hex("CipherHandler"),
+            "b": to_hex("builtins"), "t": to_hex("types"),
+            "g": to_hex("globals"), "i": to_hex("__import__"),
+            "a": to_hex("getattr"), "c": to_hex("compile"),
+            "f": to_hex("FunctionType"), "e": to_hex("eval"),
             "M": to_hex(self.method),
         }
-
+        
         if key_by_config is not None:
-            key_for_output = key_by_config
+            key_expression = key_by_config
         else:
-            key_for_output = repr(self.key)
-
+            key_expression = repr(self.key)
+        
+        key_hex = to_hex(key_expression)
+            
         result = (
-            f"(lambda d, k, h, x: "
-            f"(lambda b, i, g, c, t, f: "
-            f"f(c(g(g(i(x(h['n'])), x(h['C']))(**{{'method': x(h['M']), 'key': k}}), 'decrypt')(d), '<string>', 'exec'), t())()"
-            f")"
-            f"(__import__(x(h['b'])), "
-            f"lambda n: __import__(x(h['b'])).__dict__[x(h['i'])](n), "
-            f"lambda o, n: __import__(x(h['b'])).__dict__[x(h['a'])](o, n), "
-            f"lambda *a: __import__(x(h['b'])).__dict__[x(h['c'])](*a), "
-            f"lambda: __import__(x(h['b'])).__dict__[x(h['g'])](), "
-            f"lambda *a: __import__(x(h['b'])).__dict__[x(h['a'])](__import__(x(h['t'])), x(h['f']))(*a))"
-            f")('{encrypted_code}', {key_for_output}, {hex_map}, lambda s: bytes.fromhex(s).decode('utf-8'))"
+            f"(lambda d, kh, h, x: "
+                f"(lambda b, i, g, c, t, f, e: f(c(g(g(i(x(h['n'])), x(h['C']))(**{{'method': x(h['M']), 'key': e(x(kh))}}), 'decrypt')(d), '<string>', 'exec'), t())())"
+                f"(__import__(x(h['b'])),"
+                f"lambda n: __import__(x(h['b'])).__dict__[x(h['i'])](n),"
+                f"lambda o, n: __import__(x(h['b'])).__dict__[x(h['a'])](o, n),"
+                f"lambda *a: __import__(x(h['b'])).__dict__[x(h['c'])](*a),"
+                f"lambda: __import__(x(h['b'])).__dict__[x(h['g'])](),"
+                f"lambda *a: __import__(x(h['b'])).__dict__[x(h['a'])](__import__(x(h['t'])), x(h['f']))(*a),"
+                f"lambda s: __import__(x(h['b'])).__dict__[x(h['e'])](s))"
+            f")('{encrypted_code}', '{key_hex}', {hex_map}, lambda s: bytes.fromhex(s).decode('utf-8'))"
         )
-
+        
         try:
             with open(filename, "w") as file:
                 file.write(result)
@@ -240,34 +237,31 @@ class AsciiManager(__import__("nsdev").AnsiColors):
 
             to_hex = lambda s: s.encode("utf-8").hex()
             hex_map = {
-                "n": to_hex("nsdev"),
-                "A": to_hex("AsciiManager"),
-                "b": to_hex("builtins"),
-                "t": to_hex("types"),
-                "g": to_hex("globals"),
-                "i": to_hex("__import__"),
-                "a": to_hex("getattr"),
-                "c": to_hex("compile"),
-                "f": to_hex("FunctionType"),
+                "n": to_hex("nsdev"), "A": to_hex("AsciiManager"),
+                "b": to_hex("builtins"), "t": to_hex("types"),
+                "g": to_hex("globals"), "i": to_hex("__import__"),
+                "a": to_hex("getattr"), "c": to_hex("compile"),
+                "f": to_hex("FunctionType"), "e": to_hex("eval"),
             }
-
+            
             if key_by_config is not None:
-                key_for_output = key_by_config
+                key_expression = key_by_config
             else:
-                key_for_output = repr(self.no_format_key)
+                key_expression = repr(self.no_format_key)
 
+            key_hex = to_hex(key_expression)
+            
             result = (
-                f"(lambda d, k, h, x: "
-                f"(lambda b, i, g, c, t, f: "
-                f"f(c(g(g(i(x(h['n'])), x(h['A']))(k), 'decrypt')(d), '<string>', 'exec'), t())()"
-                f")"
-                f"(__import__(x(h['b'])), "
-                f"lambda n: __import__(x(h['b'])).__dict__[x(h['i'])](n), "
-                f"lambda o, n: __import__(x(h['b'])).__dict__[x(h['a'])](o, n), "
-                f"lambda *a: __import__(x(h['b'])).__dict__[x(h['c'])](*a), "
-                f"lambda: __import__(x(h['b'])).__dict__[x(h['g'])](), "
-                f"lambda *a: __import__(x(h['b'])).__dict__[x(h['a'])](__import__(x(h['t'])), x(h['f']))(*a))"
-                f")({str(encrypted_code)}, {key_for_output}, {hex_map}, lambda s: bytes.fromhex(s).decode('utf-8'))"
+                f"(lambda d, kh, h, x: "
+                    f"(lambda b, i, g, c, t, f, e: f(c(g(g(i(x(h['n'])), x(h['A']))(e(x(kh))), 'decrypt')(d), '<string>', 'exec'), t())())"
+                    f"(__import__(x(h['b'])),"
+                    f"lambda n: __import__(x(h['b'])).__dict__[x(h['i'])](n),"
+                    f"lambda o, n: __import__(x(h['b'])).__dict__[x(h['a'])](o, n),"
+                    f"lambda *a: __import__(x(h['b'])).__dict__[x(h['c'])](*a),"
+                    f"lambda: __import__(x(h['b'])).__dict__[x(h['g'])](),"
+                    f"lambda *a: __import__(x(h['b'])).__dict__[x(h['a'])](__import__(x(h['t'])), x(h['f']))(*a),"
+                    f"lambda s: __import__(x(h['b'])).__dict__[x(h['e'])](s))"
+                f")({str(encrypted_code)}, '{key_hex}', {hex_map}, lambda s: bytes.fromhex(s).decode('utf-8'))"
             )
 
             with open(filename, "w") as file:
