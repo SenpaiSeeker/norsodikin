e30260f8b9440f0476872c9cf5ebaf40a8ca7528
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2025-08-10 23:13:04 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,6 @@ setup(
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
@@ -33,6 +32,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.8, <3.13",
+    python_requires=">=3.9, <3.13",
     install_requires=requires,
 )
