3714d6ab863154db693ff6aede9e87514633e9e3
SenpaiSeeker <175751486+SenpaiSeeker@users.noreply.github.com>
2025-08-24 01:58:30 +0000

    Apply automatic changes

--- nsdev/monitor.py
+++ nsdev/monitor.py
@@ -9,7 +9,7 @@ class ServerMonitor:
         ram_total_gb = ram.total / (1024**3)
         ram_used_gb = ram.used / (1024**3)
         ram_percent = ram.percent
-        disk = self.psutil.disk_usage('/')
+        disk = self.psutil.disk_usage("/")
         disk_total_gb = disk.total / (1024**3)
         disk_used_gb = disk.used / (1024**3)
         disk_percent = disk.percent
@@ -20,5 +20,5 @@ class ServerMonitor:
             ram_percent=ram_percent,
             disk_total_gb=disk_total_gb,
             disk_used_gb=disk_used_gb,
-            disk_percent=disk_percent
+            disk_percent=disk_percent,
         )
