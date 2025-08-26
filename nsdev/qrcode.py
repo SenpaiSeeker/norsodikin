49fc7f8b55f1ebcfa6fd80ddfa2eaa9ad30c9ec5
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2025-08-26 16:27:15 +0700

    Update qrcode.py

--- nsdev/qrcode.py
+++ nsdev/qrcode.py
@@ -1,14 +1,13 @@
 import asyncio
 import io
+from typing import Union, Optional
 
 import qrcode
-
+from PIL import Image
+from pyzbar import pyzbar
 
 class QrCodeGenerator:
-    def __init__(self):
-        pass
-
-    def _sync_generate(self, data: str):
+    def _sync_generate(self, data: str) -> bytes:
         try:
             qr = qrcode.QRCode(
                 version=1,
@@ -21,7 +20,7 @@ class QrCodeGenerator:
             img = qr.make_image(fill_color="black", back_color="white")
 
             img_bytes = io.BytesIO()
-            img.save(img_bytes, format="PNG")
+            img.save(img_bytes, format='PNG')
             img_bytes.seek(0)
             return img_bytes.getvalue()
         except Exception as e:
@@ -32,4 +31,26 @@ class QrCodeGenerator:
         result = await loop.run_in_executor(None, self._sync_generate, data)
         if isinstance(result, Exception):
             raise result
+        return result
+
+    def _sync_read(self, image_data: Union[str, bytes, io.BytesIO]) -> Optional[str]:
+        try:
+            image = Image.open(image_data)
+            decoded_objects = pyzbar.decode(image)
+            
+            if not decoded_objects:
+                return None
+            
+            return decoded_objects[0].data.decode('utf-8')
+        except Exception as e:
+            return e
+
+    async def read(self, image_data: Union[str, bytes, io.BytesIO]) -> Optional[str]:
+        loop = asyncio.get_running_loop()
+        result = await loop.run_in_executor(None, self._sync_read, image_data)
+        
+        if isinstance(result, Exception):
+            # Jika terjadi error saat membaca gambar, lempar exception
+            raise result
+            
         return result
