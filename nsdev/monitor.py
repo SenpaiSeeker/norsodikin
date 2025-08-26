c19afb5cff0dc3f061f2a3ffe2cda96bc54f9835
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2025-08-26 11:46:50 +0700

    Update monitor.py

--- nsdev/monitor.py
+++ nsdev/monitor.py
@@ -1,19 +1,18 @@
-class ServerMonitor:
-    def __init__(self):
-        self.psutil = __import__("psutil")
-        self.SimpleNamespace = __import__("types").SimpleNamespace
+import psutil
+from types import SimpleNamespace
 
+class ServerMonitor:
     def get_stats(self):
-        cpu_percent = self.psutil.cpu_percent(interval=1)
-        ram = self.psutil.virtual_memory()
+        cpu_percent = psutil.cpu_percent(interval=1)
+        ram = psutil.virtual_memory()
         ram_total_gb = ram.total / (1024**3)
         ram_used_gb = ram.used / (1024**3)
         ram_percent = ram.percent
-        disk = self.psutil.disk_usage("/")
+        disk = psutil.disk_usage("/")
         disk_total_gb = disk.total / (1024**3)
         disk_used_gb = disk.used / (1024**3)
         disk_percent = disk.percent
-        return self.SimpleNamespace(
+        return SimpleNamespace(
             cpu_percent=cpu_percent,
             ram_total_gb=ram_total_gb,
             ram_used_gb=ram_used_gb,
