bdba9fd1dcad0107fe7180aa22340c6e1953f779
SenpaiSeeker <175751486+SenpaiSeeker@users.noreply.github.com>
2025-08-26 02:08:47 +0000

    Apply automatic changes

--- nsdev/shell.py
+++ nsdev/shell.py
@@ -1,16 +1,10 @@
 class ShellExecutor:
     def __init__(self):
         self.asyncio = __import__("asyncio")
-    
+
     async def run(self, command: str):
         process = await self.asyncio.create_subprocess_shell(
-            command,
-            stdout=self.asyncio.subprocess.PIPE,
-            stderr=self.asyncio.subprocess.PIPE
+            command, stdout=self.asyncio.subprocess.PIPE, stderr=self.asyncio.subprocess.PIPE
         )
         stdout, stderr = await process.communicate()
-        return (
-            stdout.decode('utf-8').strip(),
-            stderr.decode('utf-8').strip(),
-            process.returncode
-        )
+        return (stdout.decode("utf-8").strip(), stderr.decode("utf-8").strip(), process.returncode)
