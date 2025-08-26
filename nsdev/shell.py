f0595651f406f8e86cfcd5c80fda91eed58ce1e8
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2025-08-26 11:48:51 +0700

    Update shell.py

--- nsdev/shell.py
+++ nsdev/shell.py
@@ -1,10 +1,9 @@
-class ShellExecutor:
-    def __init__(self):
-        self.asyncio = __import__("asyncio")
+import asyncio
 
+class ShellExecutor:
     async def run(self, command: str):
-        process = await self.asyncio.create_subprocess_shell(
-            command, stdout=self.asyncio.subprocess.PIPE, stderr=self.asyncio.subprocess.PIPE
+        process = await asyncio.create_subprocess_shell(
+            command, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
         )
         stdout, stderr = await process.communicate()
         return (stdout.decode("utf-8").strip(), stderr.decode("utf-8").strip(), process.returncode)
