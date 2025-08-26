0ce06ad38522d3059c084099972f96c3624ec4e1
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2025-08-26 11:50:52 +0700

    Update translate.py

--- nsdev/translate.py
+++ nsdev/translate.py
@@ -1,8 +1,10 @@
+import asyncio
+
+import googletrans
+
 class Translator:
     def __init__(self):
-        self.asyncio = __import__("asyncio")
-        self.googletrans = __import__("googletrans")
-        self._translator = self.googletrans.Translator()
+        self._translator = googletrans.Translator()
 
     def _sync_translate(self, text: str, dest_lang: str):
         try:
@@ -12,7 +14,7 @@ class Translator:
             return e
 
     async def to(self, text: str, dest_lang: str = "en") -> str:
-        loop = self.asyncio.get_running_loop()
+        loop = asyncio.get_running_loop()
         result = await loop.run_in_executor(None, self._sync_translate, text, dest_lang)
         if isinstance(result, Exception):
             raise result
