fdb7a58b9fd904b8da9743c368518aa669534e1f
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2025-08-26 11:52:01 +0700

    Update url.py

--- nsdev/url.py
+++ nsdev/url.py
@@ -1,14 +1,17 @@
+from urllib import parse
+
+import httpx
+
 class UrlUtils:
     def __init__(self):
-        self.httpx = __import__("httpx")
-        self.urllib = __import__("urllib")
+        pass
 
     async def shorten(self, long_url: str):
-        api_url = f"http://tinyurl.com/api-create.php?url={self.urllib.parse.quote(long_url)}"
-        async with self.httpx.AsyncClient() as client:
+        api_url = f"http://tinyurl.com/api-create.php?url={parse.quote(long_url)}"
+        async with httpx.AsyncClient() as client:
             try:
                 response = await client.get(api_url)
                 response.raise_for_status()
                 return response.text
-            except self.httpx.RequestError as e:
+            except httpx.RequestError as e:
                 raise Exception(f"Gagal menghubungi layanan pemendek URL: {e}")
