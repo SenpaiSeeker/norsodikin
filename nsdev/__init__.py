ca0b2a90b151a3763af74b303c8cdb533ccb828a
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2025-08-27 10:40:22 +0700

    Update __init__.py

--- nsdev/__init__.py
+++ nsdev/__init__.py
@@ -31,7 +31,7 @@ from .vision import VisionAnalyzer
 from .web_summarizer import WebSummarizer
 from .ymlreder import YamlHandler
 
-__version__ = "1.5"
+__version__ = "1.5.1"
 __author__ = "@NorSodikin"
 
 class NsDev:
