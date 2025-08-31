a9ecee3dd4cbf6b69d3e5376b279e6c168a76205
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2025-08-31 16:46:45 +0700

    Update ratelimit.py

--- nsdev/utils/ratelimit.py
+++ nsdev/utils/ratelimit.py
@@ -2,6 +2,9 @@ import time
 from functools import wraps
 from typing import Dict, Tuple
 
+from pyrogram.types import CallbackQuery, Message
+
+
 _user_timestamps: Dict[str, Tuple[int, float]] = {}
 
 
@@ -12,9 +15,14 @@ class RateLimiter:
     def __call__(self, limit: int, per_seconds: int):
         def decorator(func):
             @wraps(func)
-            async def wrapped(client, message, *args, **kwargs):
-                user_id = message.from_user.id
-                chat_id = message.chat.id
+            async def wrapped(client, update, *args, **kwargs):
+                if isinstance(update, Message):
+                    user_id = update.from_user.id
+                elif isinstance(update, CallbackQuery):
+                    user_id = update.from_user.id
+                else:
+                    return await func(client, update, *args, **kwargs)
+
                 key = f"{user_id}:{func.__name__}"
                 
                 current_time = time.time()
@@ -26,10 +34,13 @@ class RateLimiter:
                     if time_passed < per_seconds:
                         if count >= limit:
                             time_to_wait = per_seconds - time_passed
-                            await message.reply_text(
-                                f"Anda terlalu cepat! Silakan coba lagi dalam **{int(time_to_wait)} detik**.",
-                                quote=True
-                            )
+                            error_text = f"Anda terlalu cepat! Silakan coba lagi dalam {int(time_to_wait)} detik."
+                            
+                            if isinstance(update, Message):
+                                await update.reply_text(error_text, quote=True)
+                            elif isinstance(update, CallbackQuery):
+                                await update.answer(error_text, show_alert=True)
+                            
                             return
                         
                         _user_timestamps[key] = (count + 1, last_time)
@@ -38,6 +49,6 @@ class RateLimiter:
                 else:
                     _user_timestamps[key] = (1, current_time)
                 
-                return await func(client, message, *args, **kwargs)
+                return await func(client, update, *args, **kwargs)
             return wrapped
         return decorator
