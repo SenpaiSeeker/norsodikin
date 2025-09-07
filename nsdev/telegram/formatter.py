054f12fa0d058534c72ef20028a6a5e00b4e104e
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2025-09-07 12:34:31 +0700

    Update formatter.py

--- nsdev/telegram/formatter.py
+++ nsdev/telegram/formatter.py
@@ -6,112 +6,82 @@ class MarkdownDelimiters:
     SPOILER = "||"
     CODE = "`"
     PRE = "```"
-    BLOCKQUOTE = ">"
-    BLOCKQUOTE_ESCAPE = "|>"
+    BLOCKQUOTE = "> "
+    BLOCKQUOTE_ESCAPE = "|> "
     BLOCKQUOTE_EXPANDABLE = "**>"
     BLOCKQUOTE_EXPANDABLE_END = "<**"
 
+class TextFormatter(str, MarkdownDelimiters):
+    def __new__(cls, value="", mode="html"):
+        return super().__new__(cls, value)
 
-class TextFormatter(MarkdownDelimiters):
-    def __init__(self, mode: str = "markdown"):
-        self._parts = []
-        if mode not in ["markdown", "html"]:
-            raise ValueError("Mode harus 'markdown' atau 'html'.")
+    def __init__(self, value="", mode="html"):
+        super().__init__()
         self.mode = mode
 
     def text(self, text_content: str):
-        self._parts.append(str(text_content))
-        return self
+        return TextFormatter(self + str(text_content), mode=self.mode)
 
     def bold(self, text_content: str):
         if self.mode == "html":
-            self._parts.append(f"<b>{text_content}</b>")
-        else:
-            self._parts.append(f"{self.BOLD}{text_content}{self.BOLD}")
-        return self
+            return TextFormatter(self + f"<b>{text_content}</b>", mode=self.mode)
+        return TextFormatter(self + f"{self.BOLD}{text_content}{self.BOLD}", mode=self.mode)
 
     def italic(self, text_content: str):
         if self.mode == "html":
-            self._parts.append(f"<i>{text_content}</i>")
-        else:
-            self._parts.append(f"{self.ITALIC}{text_content}{self.ITALIC}")
-        return self
+            return TextFormatter(self + f"<i>{text_content}</i>", mode=self.mode)
+        return TextFormatter(self + f"{self.ITALIC}{text_content}{self.ITALIC}", mode=self.mode)
 
     def underline(self, text_content: str):
         if self.mode == "html":
-            self._parts.append(f"<u>{text_content}</u>")
-        else:
-            self._parts.append(f"{self.UNDERLINE}{text_content}{self.UNDERLINE}")
-        return self
+            return TextFormatter(self + f"<u>{text_content}</u>", mode=self.mode)
+        return TextFormatter(self + f"{self.UNDERLINE}{text_content}{self.UNDERLINE}", mode=self.mode)
 
     def strike(self, text_content: str):
         if self.mode == "html":
-            self._parts.append(f"<s>{text_content}</s>")
-        else:
-            self._parts.append(f"{self.STRIKE}{text_content}{self.STRIKE}")
-        return self
+            return TextFormatter(self + f"<s>{text_content}</s>", mode=self.mode)
+        return TextFormatter(self + f"{self.STRIKE}{text_content}{self.STRIKE}", mode=self.mode)
 
     def spoiler(self, text_content: str):
         if self.mode == "html":
-            self._parts.append(f"<spoiler>{text_content}</spoiler>")
-        else:
-            self._parts.append(f"{self.SPOILER}{text_content}{self.SPOILER}")
-        return self
+            return TextFormatter(self + f"<spoiler>{text_content}</spoiler>", mode=self.mode)
+        return TextFormatter(self + f"{self.SPOILER}{text_content}{self.SPOILER}", mode=self.mode)
 
     def mono(self, text_content: str):
         if self.mode == "html":
-            self._parts.append(f"<code>{text_content}</code>")
-        else:
-            self._parts.append(f"{self.CODE}{text_content}{self.CODE}")
-        return self
+            return TextFormatter(self + f"<code>{text_content}</code>", mode=self.mode)
+        return TextFormatter(self + f"{self.CODE}{text_content}{self.CODE}", mode=self.mode)
 
-    def pre(self, text_content: str):
+    def pre(self, text_content: str, language: str = ""):
         if self.mode == "html":
-            self._parts.append(f"<pre>{text_content}</pre>")
-        else:
-            self._parts.append(f"{self.PRE}\n{text_content}\n{self.PRE}")
-        return self
+            return TextFormatter(self + f"<pre><code class='language-{language}'>{text_content}</code></pre>", mode=self.mode)
+        return TextFormatter(self + f"{self.PRE}{language}\n{text_content}\n{self.PRE}", mode=self.mode)
 
     def blockquote(self, text_content: str):
-        if self.mode == "html":
-            self._parts.append(f"<blockquote>{text_content}</blockquote>")
-        else:
-            lines = str(text_content).strip().split("\n")
-            formatted_lines = [f"{self.BLOCKQUOTE} {line}" for line in lines]
-            self._parts.append("\n".join(formatted_lines))
-        return self
-
+        if self.mode == 'html':
+            return TextFormatter(self + f"<blockquote>{text_content}</blockquote>", mode=self.mode)
+        
+        lines = str(text_content).strip().split('\n')
+        quoted_lines = '\n'.join(f'{self.BLOCKQUOTE}{line}' for line in lines)
+        return TextFormatter(self + quoted_lines, mode=self.mode)
+    
     def escaped_blockquote(self, text_content: str):
-        if self.mode == "html":
+        if self.mode == 'html':
             return self.blockquote(text_content)
-        else:
-            lines = str(text_content).strip().split("\n")
-            formatted_lines = [f"{self.BLOCKQUOTE_ESCAPE} {line}" for line in lines]
-            self._parts.append("\n".join(formatted_lines))
-        return self
-
+            
+        lines = str(text_content).strip().split('\n')
+        quoted_lines = '\n'.join(f'{self.BLOCKQUOTE_ESCAPE}{line}' for line in lines)
+        return TextFormatter(self + quoted_lines, mode=self.mode)
+        
     def expandable_blockquote(self, text_content: str):
-        if self.mode == "html":
-            self._parts.append(f"<blockquote expandable>{text_content}</blockquote>")
-        else:
-            self._parts.append(f"{self.BLOCKQUOTE_EXPANDABLE}{text_content}{self.BLOCKQUOTE_EXPANDABLE_END}")
-        return self
+        if self.mode == 'html':
+            return TextFormatter(self + f"<blockquote expandable>{text_content}</blockquote>", mode=self.mode)
+        return TextFormatter(self + f"{self.BLOCKQUOTE_EXPANDABLE}{text_content}{self.BLOCKQUOTE_EXPANDABLE_END}", mode=self.mode)
 
     def link(self, text_content: str, url: str):
         if self.mode == "html":
-            self._parts.append(f'<a href="{url}">{text_content}</a>')
-        else:
-            self._parts.append(f"[{text_content}]({url})")
-        return self
+            return TextFormatter(self + f'<a href="{url}">{text_content}</a>', mode=self.mode)
+        return TextFormatter(self + f"[{text_content}]({url})", mode=self.mode)
 
     def new_line(self, count: int = 1):
-        self._parts.append("\n" * count)
-        return self
-
-    def to_string(self) -> str:
-        result = "".join(self._parts)
-        self._parts = []
-        return result
-
-    def __str__(self):
-        return self.to_string()
+        return TextFormatter(self + ("\n" * count), mode=self.mode)
