24c8966c934fb3f3318a6397e32b36be71076ed9
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2025-09-18 18:40:15 +0700

    Update stt.py

--- nsdev/ai/stt.py
+++ nsdev/ai/stt.py
@@ -25,6 +25,8 @@ class SpeechToText:
         chrome_options.add_argument("--use-fake-ui-for-media-stream")
         chrome_options.add_argument("--use-fake-device-for-media-stream")
         chrome_options.add_argument("--headless=new")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
         
         try:
             service = Service(ChromeDriverManager().install())
