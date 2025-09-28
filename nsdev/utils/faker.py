4e9f08fe983914cc61a0eb7c491b5e55b63a73de
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2025-09-28 13:35:32 +0700

    Create faker.py

---
+++ nsdev/utils/faker.py
@@ -0,0 +1,21 @@
+from types import SimpleNamespace
+
+from faker import Faker
+
+
+class FakeInfoGenerator:
+    def ____init____(self, locale: str = "id_ID"):
+        self.fake = Faker(locale)
+
+    def generate(self) -> SimpleNamespace:
+        profile = self.fake.profile()
+        return SimpleNamespace(
+            name=profile.get("name"),
+            address=profile.get("address"),
+            email=profile.get("mail"),
+            job=profile.get("job"),
+            company=profile.get("company"),
+            phone_number=self.fake.phone_number(),
+            birthdate=profile.get("birthdate"),
+            website=self.fake.url(),
+        )
