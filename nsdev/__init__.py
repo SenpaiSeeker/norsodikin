ceea2477fb79bff6961ae0be064e9ac55160703b
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2025-10-24 20:25:24 +0700

    Bump version from 2.3.4 to 2.3.5

--- nsdev/__init__.py
+++ nsdev/__init__.py
@@ -68,7 +68,7 @@ from .utils.url import UrlUtils
 from .utils.weather import WeatherWttr
 from .utils.wikipedia import WikipediaSearch
 
-__version__ = "2.3.4"
+__version__ = "2.3.5"
 __author__ = "@NorSodikin"
 
 
