5275113821a373f0723c191ad14e8898d5804bb1
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2025-10-26 19:08:48 +0700

    Update downloader.py

--- nsdev/utils/downloader.py
+++ nsdev/utils/downloader.py
@@ -41,7 +41,6 @@ class MediaDownloader:
             "no_warnings": True,
             "noplaylist": True,
             "extract_flat": "in_playlist",
-            "retries": 3,
             "user_agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
         }
 
@@ -86,7 +85,6 @@ class MediaDownloader:
             "quiet": True,
             "geo_bypass": True,
             "nocheckcertificate": True,
-            "retries": 5,
             "user_agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
             "http_headers": {
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
