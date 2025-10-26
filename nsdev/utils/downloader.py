28f4d53dd286aa50047da79715526c72d1d6a284
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2025-10-26 17:51:16 +0700

    Update downloader.py

--- nsdev/utils/downloader.py
+++ nsdev/utils/downloader.py
@@ -91,7 +91,7 @@ class MediaDownloader:
             "http_headers": {
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
                 "Referer": url,
-                "Accept-Language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7",
+                "Accept-Language": "id-ID,id;q=0.9",
             },
         }
 
