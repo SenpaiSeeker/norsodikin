d2fb3f334fa64888257a62b2c84da480e90fe4ed
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2025-10-26 12:28:19 +0700

    Update __init__.py

--- nsdev/__init__.py
+++ nsdev/__init__.py
@@ -67,7 +67,7 @@ from .utils.url import UrlUtils
 from .utils.weather import WeatherWttr
 from .utils.wikipedia import WikipediaSearch
 
-__version__ = "2.3.9"
+__version__ = "2.3.10"
 __author__ = "@NorSodikin"
 
 
