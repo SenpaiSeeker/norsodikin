a91c9b70f6452e79b761ea1eefdcc23d094b9ad5
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2025-12-01 00:03:41 +0700

    Update __init__.py

--- nsdev/payment/__init__.py
+++ nsdev/payment/__init__.py
@@ -1,6 +1,6 @@
 from .cashify import PaymentCashify
 from .midtrans import PaymentMidtrans
-from .saweria import SaweriaApi
+from .saweria_api import SaweriaApi
 from .saweria_scraper import SaweriaScraper
 from .tripay import PaymentTripay
 from .violet import VioletMediaPayClient
