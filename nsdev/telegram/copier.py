ebe588010d65611f4254fdcc946392d56c1c99f5
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2025-12-12 21:05:52 +0700

    Update copier.py

--- nsdev/telegram/copier.py
+++ nsdev/telegram/copier.py
@@ -261,7 +261,6 @@ class MessageCopier:
 
             try:
                 if filter_type == "photo":
-                    await status_message.edit(f"Downloading Photo {processed + 1} tunggu ya...")
                     file_path = await self._client.download_media(msg)
                     if file_path:
                         collected_files.append(
@@ -270,7 +269,6 @@ class MessageCopier:
                         processed += 1
 
                 elif filter_type == "video":
-                    progress.reset(new_task_name=f"Downloading Video {processed + 1} tunggu ya...")
                     file_path = await self._client.download_media(msg)
                     
                     if msg.video.thumbs:
