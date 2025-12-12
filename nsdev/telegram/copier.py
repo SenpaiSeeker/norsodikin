a98e923a7416f49e050f7b32b822c2722ba7b762
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2025-12-12 20:58:18 +0700

    Update copier.py

--- nsdev/telegram/copier.py
+++ nsdev/telegram/copier.py
@@ -261,7 +261,7 @@ class MessageCopier:
 
             try:
                 if filter_type == "photo":
-                    progress.reset(new_task_name=f"Downloading Photo {processed + 1} tunggu ya...")
+                    await status_message.edit(new_task_name=f"Downloading Photo {processed + 1} tunggu ya...")
                     file_path = await self._client.download_media(msg)
                     if file_path:
                         collected_files.append(
