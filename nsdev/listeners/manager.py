e6e8db17fb4c13c50edfc7271ff29acf90aea928
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2025-12-13 14:41:36 +0700

    Update manager.py

--- nsdev/listeners/manager.py
+++ nsdev/listeners/manager.py
@@ -1,13 +1,31 @@
 import asyncio
-from typing import Optional, Union, List
-from pyrogram import Client, filters
+from typing import Optional, Union
+from pyrogram import Client
+from pyrogram.handlers import MessageHandler
 from pyrogram.types import Message
-from .exceptions import ListenerTimeout, ListenerStopped, ListenerCanceled
+from .exceptions import ListenerTimeout, ListenerCanceled
 
 class ListenerManager:
     def __init__(self, client: Client):
         self.client = client
         self.listeners = {}
+        
+        self.client.add_handler(MessageHandler(self._global_listener_handler), group=999)
+
+    async def _global_listener_handler(self, client, message):
+        if not self.listeners:
+            return
+
+        for listener_id in list(self.listeners.keys()):
+            future, check_func = self.listeners[listener_id]
+            
+            if future.done():
+                continue
+                
+            if check_func(client, message):
+                future.set_result(message)
+                if listener_id in self.listeners:
+                    del self.listeners[listener_id]
 
     async def listen(
         self,
@@ -16,14 +34,12 @@ class ListenerManager:
         filters=None,
         timeout: int = 300
     ) -> Message:
-        future = asyncio.get_running_loop().create_future()
-        
         chat_id = await self._resolve_id(chat_id)
         user_id = await self._resolve_id(user_id) if user_id else None
 
         listener_id = f"{chat_id}_{user_id}" if user_id else str(chat_id)
         
-        self.listeners[listener_id] = future
+        future = asyncio.get_running_loop().create_future()
 
         def _check(client, message):
             if message.chat.id == chat_id:
@@ -32,24 +48,17 @@ class ListenerManager:
                 if filters and not filters(client, message):
                     return False
                 return True
+            return False
 
-        @self.client.on_message(group=999)
-        async def _listener_handler(client, message):
-            if not future.done() and _check(client, message):
-                future.set_result(message)
-                self.client.remove_handler(_listener_handler, group=999)
-                if listener_id in self.listeners:
-                    del self.listeners[listener_id]
+        self.listeners[listener_id] = (future, _check)
 
         try:
             return await asyncio.wait_for(future, timeout=timeout)
         except asyncio.TimeoutError:
-            self.client.remove_handler(_listener_handler, group=999)
             if listener_id in self.listeners:
                 del self.listeners[listener_id]
             raise ListenerTimeout("Waktu tunggu habis.")
         except asyncio.CancelledError:
-            self.client.remove_handler(_listener_handler, group=999)
             if listener_id in self.listeners:
                 del self.listeners[listener_id]
             raise ListenerCanceled("Listener dibatalkan.")
@@ -82,7 +91,7 @@ class ListenerManager:
         listener_id = f"{chat_id}_{user_id}" if user_id else str(chat_id)
         
         if listener_id in self.listeners:
-            future = self.listeners[listener_id]
+            future, _ = self.listeners[listener_id]
             if not future.done():
                 future.cancel()
             del self.listeners[listener_id]
