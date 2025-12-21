1e08bacb3b220a63f6dba072084efa86bdaa11fa
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2025-12-22 02:53:00 +0700

    Create shazam.py

---
+++ nsdev/utils/shazam.py
@@ -0,0 +1,9 @@
+import io
+from shazamio import Shazam
+
+class ShazamHelper:
+    def __init__(self):
+        self.shazam = Shazam()
+
+    async def recognize(self, file_bytes: bytes):
+        return await self.shazam.recognize_song(file_bytes)
