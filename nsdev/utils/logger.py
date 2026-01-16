0e99461b1df520d1c76ef875a3c7aed697eb3bdf
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2026-01-16 10:42:44 +0700

    Update logger.py

--- nsdev/utils/logger.py
+++ nsdev/utils/logger.py
@@ -1,5 +1,4 @@
 import datetime
-import re
 import logging
 import os
 import sys
@@ -130,40 +129,13 @@ class LoggerHandler(AnsiColors):
         else:
             return self._format_classic(record)
 
-    def _strip_ansi(self, s: str) -> str:
-        return re.sub(r"\x1b\[[0-9;]*m", "", s)
-
     def print(self, message: str, isPrint: bool = True) -> Optional[str]:
-        lines = message.splitlines() or [""]
-        timestamp = self.formatTime()
-
-        clean_time = self._strip_ansi(timestamp)
-        clean_lines = [self._strip_ansi(l) for l in lines]
-
-        inner_width = max(len(clean_time), max(len(l) for l in clean_lines)) + 2
-
-        top = f"{self.CYAN}{self.box['tl']}{self.box['h'] * inner_width}{self.box['tr']}{self.RESET}"
-
-        time_line = (
-            f"{self.CYAN}{self.box['v']} "
-            f"{self.WHITE}{timestamp.ljust(inner_width - 1)}"
-            f"{self.CYAN}{self.box['v']}{self.RESET}"
+        text = (
+            f"{self.CYAN}{self.box['tl']}{self.box['h']} "
+            f"{self.WHITE}{self.formatTime()} "
+            f"{self.CYAN}{self.box['h']}{self.box['tr']}\n"
+            f"{self.CYAN}{self.box['v']} {self.WHITE}{message}{self.RESET}"
         )
-
-        content_lines = []
-        for ln in lines:
-            clean = self._strip_ansi(ln)
-            pad = inner_width - 1 - len(clean)
-            content_lines.append(
-                f"{self.CYAN}{self.box['v']} "
-                f"{self.WHITE}{ln}{' ' * pad}"
-                f"{self.CYAN}{self.box['v']}{self.RESET}"
-            )
-
-        bottom = f"{self.CYAN}{self.box['bl']}{self.box['h'] * inner_width}{self.box['br']}{self.RESET}"
-
-        text = "\n".join([top, time_line] + content_lines + [bottom])
-
         if isPrint:
             print(f"\033[2K{text}")
         else:
