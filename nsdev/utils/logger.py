729683893736315c50de78c1853cdbf7ca36bf93
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2026-01-18 14:41:19 +0700

    Update logger.py

--- nsdev/utils/logger.py
+++ nsdev/utils/logger.py
@@ -135,8 +135,8 @@ class LoggerHandler(AnsiColors):
             f"{self.CYAN}{self.box['tl']}{self.box['h']} "
             f"{self.WHITE}{self.formatTime()} "
             f"{self.CYAN}{self.box['h']*(width-len(self.formatTime()))}{self.box['tr']}\n"
-            f"{self.CYAN}{self.box['sep']} {self.WHITE}{message}{self.RESET}\n"
-            f"{self.CYAN}{self.box['bl']}{self.box['h']*width}{self.box['br']}"
+            f"{self.CYAN}{self.box['sep']} {self.WHITE}{message}\n"
+            f"{self.CYAN}{self.box['bl']}{self.box['h']*width}{self.box['br']}{self.RESET}"
         )
         if isPrint:
             print(f"\033[2K{text}")
