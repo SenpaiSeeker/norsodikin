ecd4eafb1028f6b1dd0d2864b889365804442a61
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2026-01-30 09:36:17 +0700

    Update logger.py

--- nsdev/utils/logger.py
+++ nsdev/utils/logger.py
@@ -13,22 +13,23 @@ class LoggerHandler(AnsiColors):
         super().__init__()
         self.tz = zoneinfo.ZoneInfo(options.get("tz", "Asia/Jakarta"))
         self.datefmt = options.get("datefmt", "%Y-%m-%d %H:%M:%S %Z")
+        self.show_emoji = options.get("show_emoji", True)
         
         self.colors = {
-            "INFO": self.GREEN,
-            "DEBUG": self.BLUE,
-            "WARNING": self.YELLOW,
-            "ERROR": self.RED,
-            "CRITICAL": self.MAGENTA,
+            "INFO": self.LIGHT_GREEN,
+            "DEBUG": self.LIGHT_BLUE,
+            "WARNING": self.LIGHT_YELLOW,
+            "ERROR": self.LIGHT_RED,
+            "CRITICAL": self.LIGHT_MAGENTA,
             "SUCCESS": self.LIGHT_GREEN,
-            "TIME": self.CYAN,
+            "TIME": self.WHITE,
             "MODULE": self.LIGHT_CYAN,
-            "FUNC": self.PURPLE,
-            "LINE": self.ORANGE,
-            "PIPE": self.GRAY,
-            "BOX": self.GRAY,
+            "FUNC": self.LIGHT_PURPLE,
+            "LINE": self.LIGHT_ORANGE,
+            "PIPE": self.LIGHT_GRAY,
+            "BOX": self.LIGHT_GRAY,
             "RESET": self.RESET,
-            "TEXT": self.WHITE
+            "TEXT": self.LIGHT_WHITE
         }
         
         self.box = {
@@ -39,8 +40,8 @@ class LoggerHandler(AnsiColors):
             "h": "─",
             "v": "│",
             "sep": "├─",
-            "obr": "[",
-            "cbr": "]",
+            "obr": "[ ",
+            "cbr": " ]",
             "pipe": "|"
         }
 
@@ -67,7 +68,7 @@ class LoggerHandler(AnsiColors):
         
         box_c = self.colors["BOX"]
         time_c = self.colors["TIME"]
-        lvl_c = self.colors.get(level, self.RESET)
+        lvl_c = self.colors.get(level, self.LIGHT_GREEN)
         mod_c = self.colors["MODULE"]
         func_c = self.colors["FUNC"]
         ln_c = self.colors["LINE"]
@@ -75,20 +76,20 @@ class LoggerHandler(AnsiColors):
         text_c = self.colors["TEXT"]
         rst = self.colors["RESET"]
 
-        top_bar_len = width - len(timestamp) - 6 
+        top_bar_len = width - len(timestamp) - 8
         if top_bar_len < 0: top_bar_len = 0
         
         header = (
-            f"{box_c}{self.box['tl']}{self.box['h']}{self.box['obr']} "
-            f"{time_c}{timestamp}{rst} "
+            f"{box_c}{self.box['tl']}{self.box['h']}{self.box['obr']}"
+            f"{time_c}{timestamp}{rst}"
             f"{box_c}{self.box['cbr']}{self.box['h'] * top_bar_len}{self.box['tr']}{rst}"
         )
         
         content = (
             f"{box_c}{self.box['sep']} {self.box['obr']}"
-            f"{lvl_c}{level:<8}{rst}"
+            f"{lvl_c}{level}{rst}"
             f"{box_c}{self.box['cbr']} "
-            f"{pipe_c}{self.box['pipe']}  "
+            f"{pipe_c}{self.box['pipe']} "
             f"{mod_c}{module}{rst}:"
             f"{func_c}{func}{rst}:"
             f"{ln_c}{line}{rst} "
@@ -101,7 +102,7 @@ class LoggerHandler(AnsiColors):
         return f"{header}\n{content}\n{footer}"
 
     def print(self, message: str, isPrint: bool = True) -> Optional[str]:
-        width = min(self._get_terminal_width(), 80)
+        width = min(self._get_terminal_width(), 100)
         timestamp = self.formatTime()
         
         box_c = self.colors["BOX"]
@@ -109,12 +110,12 @@ class LoggerHandler(AnsiColors):
         text_c = self.colors["TEXT"]
         rst = self.colors["RESET"]
 
-        top_bar_len = width - len(timestamp) - 6
+        top_bar_len = width - len(timestamp) - 8
         if top_bar_len < 0: top_bar_len = 0
 
         text = (
-            f"{box_c}{self.box['tl']}{self.box['h']}{self.box['obr']} "
-            f"{time_c}{timestamp}{rst} "
+            f"{box_c}{self.box['tl']}{self.box['h']}{self.box['obr']}"
+            f"{time_c}{timestamp}{rst}"
             f"{box_c}{self.box['cbr']}{self.box['h'] * top_bar_len}{self.box['tr']}\n"
             f"{box_c}{self.box['sep']} {text_c}{message}{rst}\n"
             f"{box_c}{self.box['bl']}{self.box['h'] * (width - 2)}{self.box['br']}{rst}"
@@ -128,7 +129,7 @@ class LoggerHandler(AnsiColors):
     def banner(self, title: str, subtitle: str = ""):
         width = min(self._get_terminal_width(), 80)
         box_c = self.colors["BOX"]
-        title_c = self.colors["CYAN"]
+        title_c = self.colors["LIGHT_CYAN"]
         sub_c = self.colors["WHITE"]
         rst = self.colors["RESET"]
         
