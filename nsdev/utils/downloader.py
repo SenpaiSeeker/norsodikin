99ce825d9e270948c318a74e0d835b049a28ac26
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2026-02-08 19:21:16 +0700

    Update downloader.py

--- nsdev/utils/downloader.py
+++ nsdev/utils/downloader.py
@@ -80,7 +80,7 @@ class MediaDownloader:
                     asyncio.run_coroutine_threadsafe(progress_callback(d["downloaded_bytes"], total_bytes), loop)
 
         opts = {
-            "outtmpl": os.path.join(self.download_path, "%(id)s.%(ext)s"),
+            "outtmpl": os.path.join(self.download_path, "%(id).50s.%(ext)s"),
             "restrictfilenames": True,
             "no_warnings": True,
             "noplaylist": True,
