6dd030270ce376325c076d632eca23d7c4888277
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2026-02-08 16:23:42 +0700

    Update downloader.py

--- nsdev/utils/downloader.py
+++ nsdev/utils/downloader.py
@@ -80,7 +80,8 @@ class MediaDownloader:
                     asyncio.run_coroutine_threadsafe(progress_callback(d["downloaded_bytes"], total_bytes), loop)
 
         opts = {
-            "outtmpl": os.path.join(self.download_path, "%(id)s.%(ext)s"),
+            "outtmpl": os.path.join(self.download_path, "%(id).50s.%(ext)s"),
+            "restrictfilenames": True,
             "no_warnings": True,
             "noplaylist": True,
             "quiet": True,
@@ -90,8 +91,7 @@ class MediaDownloader:
             "source_address": "0.0.0.0",
             "extractor_args": {
                 "youtube": {
-                    "player_client": ["android"],
-                    "skip": ["webpage", "auth_check"],
+                    "player_client": ["android"]
                 }
             },
         }
