9d1bc631e98f5f8ea452a5419b753ba49986ded5
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2026-04-02 21:59:41 +0700

    Update __init__.py

--- nsdev/__init__.py
+++ nsdev/__init__.py
@@ -215,16 +215,15 @@ class NsDev:
         )
 
 
-@property
-def ns(self) -> NsDev:
-    if not hasattr(self, "_nsdev_instance"):
-        self._nsdev_instance = NsDev(self)
-    return self._nsdev_instance
-
-
 try:
     from pyrogram import Client
 
-    Client.ns = ns
-except Exception:
+    def get_ns(self) -> NsDev:
+        if not hasattr(self, "_nsdev_instance"):
+            self._nsdev_instance = NsDev(self)
+        return self._nsdev_instance
+
+    Client.ns = property(get_ns)
+
+except ImportError:
     pass
